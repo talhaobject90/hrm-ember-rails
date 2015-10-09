00b022b879fd0db12f6a0a109868289ccea790c7
Mohammed Talha <talhaqqq@gmail.com>
2015-10-09 20:34:56 +0530

    After merging

---
+++ hrm-ember/app/controllers/application.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+
+  actions: {
+    invalidateSession: function() {
+      this.get('session').invalidate();
+    }
+  }
+
+
+});
