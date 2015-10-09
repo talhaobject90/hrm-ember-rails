f1ee94725240271442d3b305b94cdfe971b3adb2
Mohammed Talha <talhaqqq@gmail.com>
2015-10-09 19:03:25 +0530

    After employee controller is created

--- hrm-ember/app/controllers/login.js
+++ hrm-ember/app/controllers/login.js
@@ -2,5 +2,19 @@ import Ember from 'ember';
 import LoginControllerMixin from 'simple-auth/mixins/login-controller-mixin';
 
 export default Ember.Controller.extend(LoginControllerMixin, {
-  authenticator: 'simple-auth-authenticator:devise'
+  authenticator: 'simple-auth-authenticator:devise',
+    actions: {
+      invalidateSession: function() {
+        var self = this;
+
+        return Ember.$.ajax({
+          url: '/users/sign_out',
+          type: 'DELETE'
+        }).then(function() {
+          return self._super();
+        });
+      }
+    }
+
+
 });
